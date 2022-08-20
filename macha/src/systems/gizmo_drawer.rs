e12c73c4e68be6dc64ea51ad97d7a642f18f23ec
Ithyx <lamidey.m@gmail.com>
2022-08-20 16:48:44 -0400

    Added gizmo scaling

--- macha/src/systems/gizmo_drawer.rs
+++ macha/src/systems/gizmo_drawer.rs
@@ -1,6 +1,6 @@
 use bevy_ecs::prelude::{Query, Res};
 use egui::LayerId;
-use egui_gizmo::Gizmo;
+use egui_gizmo::{Gizmo, GizmoVisuals};
 use morrigu::{
     components::{camera::Camera, transform::Transform},
     vector_type::Mat4,
@@ -23,11 +23,23 @@ pub fn draw_gizmo(
                 ui.with_layer_id(LayerId::background(), |ui| {
                     let is_snapping_enabled = window_input.held_shift();
 
+                    let size = camera.size();
+                    let scaling = if size.x < size.y {
+                        size.x / 1280.0
+                    } else {
+                        size.y / 720.0
+                    };
+                    let mut visuals = GizmoVisuals::default();
+                    visuals.gizmo_size *= 1.2 * scaling;
+                    visuals.stroke_width *= 1.2 * (((scaling - 1.0) * 0.3) + 1.0);
+                    visuals.inactive_alpha += 0.25;
+
                     let gizmo = Gizmo::new("Selected entity gizmo")
                         .view_matrix(*camera.view())
                         .projection_matrix(*camera.projection())
                         .model_matrix(*transform.matrix())
                         .mode(macha_options.preferred_gizmo)
+                        .visuals(visuals)
                         .snap_distance(0.5)
                         .snap_angle(f32::to_radians(45.0))
                         .snap_scale(0.5)
